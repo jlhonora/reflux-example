d7f23f6044e2e27550b5703a052588e95a5a7011
jlhonora <jlhonora@ing.puc.cl>
2015-09-08 00:20:14 -0300

    Cleanup

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,2 @@
 var Router = require('./router');
-
-var ImageActions = require('./actions/imageactions');
-
-//setInterval(function() {
-//      ImageActions.fetchList();
-//}, 5000);
-
 Router.start();
